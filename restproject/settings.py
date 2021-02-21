aa47eaee453b655fcd2bbb01c340bab866754b6f
devendra <devpatel1837@gmail.com>
2021-02-21 16:37:51 -0500

    fix path for static files

--- restproject/settings.py
+++ restproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 # STATIC_URL = '/static/'
-STATIC_ROOT = '/static/' # for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # for heroku
 GRAPHENE = {
     "SCHEMA": "devrestapp.schema.schema",
     "MIDDLEWARE": {
