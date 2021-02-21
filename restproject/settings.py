10394c9b0fbff1ed0df02c95f969d2ee60dae2fe
devendra <devpatel1837@gmail.com>
2021-02-21 16:34:28 -0500

    static root

--- restproject/settings.py
+++ restproject/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_ROOT = '/static/' # for heroku
 GRAPHENE = {
     "SCHEMA": "devrestapp.schema.schema",
     "MIDDLEWARE": {
